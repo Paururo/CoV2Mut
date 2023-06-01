3b09533768fb7d7ba31398c4eb1afa28c1ed5a10
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-06-01 20:54:50 +0200

    add argparse arguments

--- spike_mutator.py
+++ spike_mutator.py
@@ -7,7 +7,20 @@ import os
 import subprocess
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Generate Spike with mutations")
+    parser = argparse.ArgumentParser(description = "Generate Spike protein sequences with mutations from provided reference sequence and variant list.")
+    
+    parser.add_argument("-r", "--reference_file", required=True,
+                        help="Path to the FASTA file containing the reference sequence.")
+    parser.add_argument("-v", "--variant_list", required=True,
+                        help="Path to the list of variants of interest.")
+    parser.add_argument("-n", "--name", required=True,
+                        help="Desired name for the output file.")
+    parser.add_argument("-g", "--gaps", action="store_true",
+                        help="Include this flag to remove gaps in the output sequence(s).")
+    parser.add_argument("-s", "--split", action="store_true",
+                        help="Include this flag to split the output into separate FASTA files for each sequence.")
+    args = parser.parse_args()
+    return args
 
 
 def main():
