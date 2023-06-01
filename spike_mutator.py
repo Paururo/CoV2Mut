bc9b0353b031cac020052a24b1c3ba4c5f523186
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-06-01 20:51:17 +0200

    add def main and argparse

--- spike_mutator.py
+++ spike_mutator.py
@@ -5,3 +5,13 @@ from Bio import SeqIO
 import argparse
 import os
 import subprocess
+
+def parse_args():
+    parser = argparse.ArgumentParser(description = "Generate Spike with mutations")
+
+
+def main():
+    args = parse_args()
+
+if __name__ == 'main':
+    main()
